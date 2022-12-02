09f24233f070e5b1a560553158b493611a826914
David Heinemann <david@dheinemann.com>
2022-12-02 20:35:17 +1000

    Add 2022 Day 2 Part 2 solution

--- 2022/csharp/day2/Program.cs
+++ 2022/csharp/day2/Program.cs
@@ -1,40 +1,83 @@
 ﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 
-var moves = new Dictionary<char, Move>
-{
-    { 'A', Move.Rock     },
-    { 'B', Move.Paper    },
-    { 'C', Move.Scissors },
-    { 'X', Move.Rock     },
-    { 'Y', Move.Paper    },
-    { 'Z', Move.Scissors },
-};
-
-var outcomes = new Dictionary<(Move, Move), Outcome>
-{
-    { (Move.Rock,     Move.Rock),     Outcome.Draw },
-    { (Move.Rock,     Move.Paper),    Outcome.Win  },
-    { (Move.Rock,     Move.Scissors), Outcome.Lose },
-    { (Move.Paper,    Move.Rock),     Outcome.Lose },
-    { (Move.Paper,    Move.Paper),    Outcome.Draw },
-    { (Move.Paper,    Move.Scissors), Outcome.Win  },
-    { (Move.Scissors, Move.Rock),     Outcome.Win  },
-    { (Move.Scissors, Move.Paper),    Outcome.Lose },
-    { (Move.Scissors, Move.Scissors), Outcome.Draw }
-};
-
-int totalScore = 0;
+int partATotalScore = 0;
+int partBTotalScore = 0;
 foreach (string round in File.ReadAllLines(filePath))
 {
-    Move theirMove = moves[round[0]];
-    Move myMove    = moves[round[2]];
-    totalScore += (int)myMove;
-
-    Outcome outcome = outcomes[(theirMove, myMove)];
-    totalScore += (int)outcome;
+    partATotalScore += CalculatePartAScore(round);
+    partBTotalScore += CalculatePartBScore(round);
 }
 
-Console.WriteLine($"Total score: {totalScore} points.");
+Console.WriteLine($"Part A total score: {partATotalScore} points.");
+Console.WriteLine($"Part B total score: {partBTotalScore} points.");
+
+int CalculatePartAScore(string round)
+{
+    var moves = new Dictionary<char, Move>
+    {
+        { 'A', Move.Rock     },
+        { 'B', Move.Paper    },
+        { 'C', Move.Scissors },
+        { 'X', Move.Rock     },
+        { 'Y', Move.Paper    },
+        { 'Z', Move.Scissors },
+    };
+
+    var outcomes = new Dictionary<(Move, Move), Outcome>
+    {
+        { (Move.Rock,     Move.Rock),     Outcome.Draw },
+        { (Move.Rock,     Move.Paper),    Outcome.Win  },
+        { (Move.Rock,     Move.Scissors), Outcome.Lose },
+        { (Move.Paper,    Move.Rock),     Outcome.Lose },
+        { (Move.Paper,    Move.Paper),    Outcome.Draw },
+        { (Move.Paper,    Move.Scissors), Outcome.Win  },
+        { (Move.Scissors, Move.Rock),     Outcome.Win  },
+        { (Move.Scissors, Move.Paper),    Outcome.Lose },
+        { (Move.Scissors, Move.Scissors), Outcome.Draw }
+    };
+
+    Move theirMove = moves[round[0]];
+    Move myMove    = moves[round[2]];
+
+    Outcome outcome = outcomes[(theirMove, myMove)];
+    return (int)myMove + (int)outcome;
+}
+
+int CalculatePartBScore(string round)
+{
+    var moves = new Dictionary<char, Move>
+    {
+        { 'A', Move.Rock     },
+        { 'B', Move.Paper    },
+        { 'C', Move.Scissors }
+    };
+    
+    var outcomes = new Dictionary<char, Outcome>
+    {
+        { 'X', Outcome.Lose },
+        { 'Y', Outcome.Draw },
+        { 'Z', Outcome.Win  }
+    };
+
+    var targetMoves = new Dictionary<(Move, Outcome), Move>
+    {
+        { (Move.Rock,     Outcome.Draw), Move.Rock     },
+        { (Move.Rock,     Outcome.Win),  Move.Paper    },
+        { (Move.Rock,     Outcome.Lose), Move.Scissors },
+        { (Move.Paper,    Outcome.Lose), Move.Rock     },
+        { (Move.Paper,    Outcome.Draw), Move.Paper    },
+        { (Move.Paper,    Outcome.Win),  Move.Scissors },
+        { (Move.Scissors, Outcome.Win),  Move.Rock     },
+        { (Move.Scissors, Outcome.Lose), Move.Paper    },
+        { (Move.Scissors, Outcome.Draw), Move.Scissors }
+    };
+
+    Move    theirMove      = moves[round[0]];
+    Outcome desiredOutcome = outcomes[round[2]];
+    Move    myMove         = targetMoves[(theirMove, desiredOutcome)];
+
+    return (int)myMove + (int)desiredOutcome;
+}
 
 internal enum Move
 {
