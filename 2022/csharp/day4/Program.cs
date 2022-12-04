97746497afc7d539f80fe5b4f2608c0287742443
David Heinemann <david@dheinemann.com>
2022-12-04 15:31:47 +1000

    Add 2022 Day 4 Part 1 solution

---
+++ 2022/csharp/day4/Program.cs
@@ -0,0 +1,18 @@
+﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+
+int subsetCount = 0;
+foreach (string pair in File.ReadAllLines(filePath))
+{
+    string[] elves = pair.Split(',');
+
+    int[] elf1Sections = elves[0].Split('-').Select(s => Convert.ToInt32(s)).ToArray();
+    int[] elf2Sections = elves[1].Split('-').Select(s => Convert.ToInt32(s)).ToArray();
+
+    bool elf1IsSubsetOf2 = elf1Sections[0] >= elf2Sections[0] && elf1Sections[1] <= elf2Sections[1];
+    bool elf2IsSubsetOf1 = elf2Sections[0] >= elf1Sections[0] && elf2Sections[1] <= elf1Sections[1];
+
+    if (elf1IsSubsetOf2 || elf2IsSubsetOf1)
+        subsetCount++;
+}
+
+Console.WriteLine($"Part 1: {subsetCount}");
