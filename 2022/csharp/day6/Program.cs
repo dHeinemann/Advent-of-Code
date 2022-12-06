d00ad7d7512cd01bd3d237f08edb70390904eb02
David Heinemann <david@dheinemann.com>
2022-12-06 20:19:03 +1000

    Add 2022 Day 6 Part 1 solution

---
+++ 2022/csharp/day6/Program.cs
@@ -0,0 +1,12 @@
+﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+string input = File.ReadAllText(filePath);
+
+for (int i = 4; i < input.Length; i++)
+{
+    var characters = new HashSet<char>(input[(i-4)..i]);
+    if (characters.Count == 4)
+    {
+        Console.WriteLine($"Part 1: {i}");
+        break;
+    }
+}
