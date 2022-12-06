ef1c290618a81180549b7d30249ae9841908fb7a
David Heinemann <david@dheinemann.com>
2022-12-06 20:21:22 +1000

    Add 2022 Day 6 Part 2 solution

--- 2022/csharp/day6/Program.cs
+++ 2022/csharp/day6/Program.cs
@@ -1,12 +1,27 @@
 ﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 string input = File.ReadAllText(filePath);
 
+int? part1Answer = null;
+int? part2Answer = null;
 for (int i = 4; i < input.Length; i++)
 {
-    var characters = new HashSet<char>(input[(i-4)..i]);
-    if (characters.Count == 4)
+    if (part1Answer == null)
     {
-        Console.WriteLine($"Part 1: {i}");
-        break;
+        var characters = new HashSet<char>(input[(i - 4)..i]);
+        if (characters.Count == 4)
+            part1Answer = i;
     }
+
+    if (part2Answer == null && i >= 14)
+    {
+        var characters = new HashSet<char>(input[(i - 14)..i]);
+        if (characters.Count == 14)
+            part2Answer = i;
+    }
+
+    if (part1Answer != null && part2Answer != null)
+        break;
 }
+
+Console.WriteLine($"Part 1: {part1Answer}");
+Console.WriteLine($"Part 2: {part2Answer}");
