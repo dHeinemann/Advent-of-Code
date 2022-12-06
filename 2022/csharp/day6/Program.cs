3600a8de51fab24f80840ebff5bf961c84ef0883
David Heinemann <david@dheinemann.com>
2022-12-06 21:50:40 +1000

    Add additional LINQ-based solution for 2022 Day 6

--- 2022/csharp/day6/Program.cs
+++ 2022/csharp/day6/Program.cs
@@ -1,6 +1,7 @@
 ﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 string input = File.ReadAllText(filePath);
 
+// Standard solution
 for (int i = 4; i < input.Length; i++)
     if (input[(i - 4)..i].Distinct().Count() == 4)
     {
@@ -14,3 +15,20 @@ for (int i = 14; i < input.Length; i++)
         Console.WriteLine($"Part 2: {i}");
         break;
     }
+
+// Bonus LINQ solution
+Console.WriteLine($"Part 1: {FindStart(input,  4)} (LINQ)");
+Console.WriteLine($"Part 2: {FindStart(input, 14)} (LINQ)");
+
+int FindStart(string s, int length)
+{
+    return s.Select((_, i) => new
+    {
+        Index = i,
+        Input = i >= length
+            ? s[(i - length)..i]
+            : string.Empty
+    })
+    .First(i => i.Input.Distinct().Count() == length)
+    .Index;
+}
