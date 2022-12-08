2eae48b67e0711d60a85f4307780506867fa1bc0
David Heinemann <david@dheinemann.com>
2022-12-08 22:34:57 +1000

    Add 2022 Day 8 Part 2 solution

--- 2022/csharp/day8/Program.cs
+++ 2022/csharp/day8/Program.cs
@@ -1,55 +1,119 @@
-string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 
 int[,] trees = ParseInput(File.ReadAllText(filePath));
-var visible = new HashSet<(int, int)>();
 
-for (int col = 0; col < trees.GetLength(0); col++)
-{
-    int last = -1;
-    for (int row = 0; row < trees.GetLength(1); row++)
-    {
-        if (trees[col, row] > last)
-        {
-            visible.Add((col, row));
-            last = trees[col, row];
-        }
-    }
-    
-    last = -1;
-    for (int row = trees.GetLength(1) - 1; row >= 0; row--)
-    {
-        if (trees[col, row] > last)
-        {
-            visible.Add((col, row));
-            last = trees[col, row];
-        }
-    }
-}
+Console.WriteLine($"Part 1: {CalculatePart1()}");
+Console.WriteLine($"Part 2: {CalculatePart2()}");
 
-for (int row = 0; row < trees.GetLength(1); row++)
+int CalculatePart1()
 {
-    int last = -1;
+    var visible = new HashSet<(int, int)>();
     for (int col = 0; col < trees.GetLength(0); col++)
     {
-        if (trees[col, row] > last)
+        int last = -1;
+        for (int row = 0; row < trees.GetLength(1); row++)
         {
-            visible.Add((col, row));
-            last = trees[col, row];
+            if (trees[col, row] > last)
+            {
+                visible.Add((col, row));
+                last = trees[col, row];
+            }
+        }
+
+        last = -1;
+        for (int row = trees.GetLength(1) - 1; row >= 0; row--)
+        {
+            if (trees[col, row] > last)
+            {
+                visible.Add((col, row));
+                last = trees[col, row];
+            }
+        }
+    }
+
+    for (int row = 0; row < trees.GetLength(1); row++)
+    {
+        int last = -1;
+        for (int col = 0; col < trees.GetLength(0); col++)
+        {
+            if (trees[col, row] > last)
+            {
+                visible.Add((col, row));
+                last = trees[col, row];
+            }
+        }
+
+        last = -1;
+        for (int col = trees.GetLength(0) - 1; col >= 0; col--)
+        {
+            if (trees[col, row] > last)
+            {
+                visible.Add((col, row));
+                last = trees[col, row];
+            }
+        }
+    }
+
+    return visible.Count;
+}
+
+int CalculatePart2()
+{
+    int topScore = 0;
+    for (int col = 0; col < trees.GetLength(0); col++)
+    {
+        for (int row = 0; row < trees.GetLength(1); row++)
+        {
+            int score = CalculateScore(col, row);
+            if (score > topScore)
+                topScore = score;
+        }
+    }
+
+    return topScore;
+}
+
+int CalculateScore(int x, int y)
+{
+    int[] distances = new int[4];
+    for (int col = x+1; col < trees.GetLength(0); col++)
+    {
+        if (trees[col, y] >= trees[x, y] || col == trees.GetLength(0) - 1)
+        {
+            distances[0] = col - x;
+            break;
         }
     }
     
-    last = -1;
-    for (int col = trees.GetLength(0) - 1; col >= 0; col--)
+    for (int col = x-1; col >= 0; col--)
     {
-        if (trees[col, row] > last)
+        if (trees[col, y] >= trees[x, y] || col == 0)
         {
-            visible.Add((col, row));
-            last = trees[col, row];
+            distances[1] = x - col;
+            break;
         }
     }
-}
+    
+    for (int row = y+1; row < trees.GetLength(1); row++)
+    {
+        if (trees[x, row] >= trees[x, y] || row == trees.GetLength(1) - 1)
+        {
+            distances[2] = row - y;
+            break;
+        }
+    }
+    
+    for (int row = y-1; row >= 0; row--)
+    {
+        if (trees[x, row] >= trees[x, y] || row == 0)
+        {
+            distances[3] = y - row;
+            break;
+        }
+    }
 
-Console.WriteLine($"Part 1: {visible.Count}");
+    return distances.Aggregate(1, (a, b) => a * b);
+}
 
 int[,] ParseInput(string input)
 {
